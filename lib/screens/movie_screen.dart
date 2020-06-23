4ccae617207b042ad56e10949936d9f5d8aca98d
Sooraj S Nair <nairsooraj2000@gmail.com>
2020-06-23 18:50:55 +0530

    on the welcome screen background, a gradient and a doodle is set over the gradient

--- lib/screens/movie_screen.dart
+++ lib/screens/movie_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   static String id = "movie_screen";
+
+  @override
+  _MovieScreenState createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,9 +1,54 @@
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   static String id = "welcome_screen.dart";
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Expanded(
+              child: Stack(
+                alignment: AlignmentDirectional.center,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Color(0xFFFF9966),
+                        Color(0xFFFF5E62),
+                      ], stops: [
+                        .3,
+                        1
+                      ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.1), BlendMode.dstIn),
+                          image: AssetImage(
+                            'images/doodle.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
