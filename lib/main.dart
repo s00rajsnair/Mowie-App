fef7b369d3a8c509517564a394d308cd92b4976c
Sooraj S Nair <nairsooraj2000@gmail.com>
2020-06-23 20:24:19 +0530

    custom font imported and added to the title mowie, created the textfield and styled it. So, the welcome screen interface is complete now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/movie_screen.dart';
 
@@ -9,6 +10,10 @@ void main() {
 class MowieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       initialRoute: WelcomeScreen.id,
       routes: {
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -29,20 +29,86 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                       ], begin: Alignment.topLeft, end: Alignment.bottomRight),
                     ),
                   ),
-                  Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(
-                              Colors.black.withOpacity(0.1), BlendMode.dstIn),
-                          image: AssetImage(
-                            'images/doodle.png',
-                          ),
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.1), BlendMode.dstIn),
+                        image: AssetImage(
+                          'images/doodle.png',
                         ),
                       ),
                     ),
                   ),
+                  Positioned(
+                    top: 250,
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              'mowie',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'SourceSansPro',
+                                shadows: [
+                                  Shadow(
+                                    offset: Offset(0, 1),
+                                    blurRadius: 5.0,
+                                    color: Color.fromARGB(50, 0, 0, 0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 100,
+                            width: 300,
+                            child: TextField(
+                              enabled: true,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(100.0)),
+                                  borderSide: BorderSide(
+                                      color: Colors.black54, width: 1),
+                                ),
+                                disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(100.0),
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(100.0),
+                                  ),
+                                ),
+                                contentPadding:
+                                    EdgeInsets.symmetric(vertical: 0),
+                                hintText: 'Enter a movie name',
+                                hintStyle: TextStyle(
+                                  color: Colors.black38,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
