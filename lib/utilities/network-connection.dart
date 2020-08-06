336de6a9e2beb2d77172dd1c944df49b22aa6ef7
Sooraj S Nair <nairsooraj2000@gmail.com>
2020-08-06 15:37:37 +0530

    added network connectivity permission for the android version in AndroiManifest.xml and added an else statement network-conection.dart to avoid any unforeseen bugs

--- lib/utilities/network-connection.dart
+++ lib/utilities/network-connection.dart
@@ -5,7 +5,8 @@ Future isConnected() async {
     final result = await InternetAddress.lookup('google.com');
     if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
       return true;
-    }
+    } else
+      return false;
   } on SocketException catch (_) {
     return false;
   }
